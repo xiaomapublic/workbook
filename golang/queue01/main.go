4933486bebaec136e270f99e1bbe8654dba26d68
maya <maya1759@stnts.com>
2020-05-09 15:07:18 +0800

    queue01

--- golang/queue01/main.go
+++ golang/queue01/main.go
@@ -1,53 +1,83 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"math/rand"
-	"time"
+	"os"
 )
 
 type Queue struct {
-	front int
-	rear  int
-	queue []int
+	maxSize int
+	front   int
+	rear    int
+	queue   []int
 }
 
 //切片实现队列
 func main() {
-	var queue Queue
-	queue.front = -1
-	queue.rear = -1
-	queue.queue = make([]int, 0)
-	randObj := rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	for i := 0; i < 10; i++ {
-		queue.addQueue(randObj.Intn(100))
+	queue := &Queue{
+		maxSize: 5,
+		front:   -1,
+		rear:    -1,
+		queue:   make([]int, 0),
 	}
-	fmt.Println(queue.queue, queue.front, queue.rear)
 
+	var key string
 	for {
-		if data, ok := queue.getQueue(); ok {
-			fmt.Printf("%d ", data)
-		} else {
-			fmt.Println("\n队列消耗完毕")
-			break
+		fmt.Printf("front: %d, rear: %d \n", queue.front, queue.rear)
+		fmt.Println("1、添加数据入队列，请输入add")
+		fmt.Println("2、获取队列数据，请输入get")
+		fmt.Println("3、展示队列，请输入show")
+		fmt.Println("4、退出系统，请输入exit")
+
+		fmt.Scanln(&key)
+		switch key {
+		case "add":
+			fmt.Println("请输入需要添加的数字")
+			var num int
+			fmt.Scanln(&num)
+			if ok := queue.addQueue(num); ok != nil {
+				fmt.Println(ok)
+			}
+		case "get":
+			if data, ok := queue.getQueue(); ok != nil {
+				fmt.Println(ok)
+			} else {
+				fmt.Println(data)
+			}
+		case "show":
+			queue.showQueue()
+		case "exit":
+			os.Exit(0)
 		}
+		fmt.Println()
 	}
-	fmt.Println(queue.queue, queue.front, queue.rear)
 }
 
-//队列中添加数据
-func (queue *Queue) addQueue(data int) {
-	queue.queue = append(queue.queue, data)
+//队列添加数据
+func (queue *Queue) addQueue(data int) (error error) {
+
+	if queue.rear+1 == queue.maxSize {
+		return errors.New("queue full")
+	}
 	queue.rear++
+	queue.queue = append(queue.queue, data)
+	return
 }
 
-//队列中获取数据
-func (queue *Queue) getQueue() (data int, ok bool) {
+//队列弹出数据
+func (queue *Queue) getQueue() (data int, err error) {
 	if queue.front == queue.rear {
-		return 0, false
-	} else {
-		queue.front++
-		return queue.queue[queue.front], true
+		return -1, errors.New("queue empty")
 	}
+	queue.front++
+	return queue.queue[queue.front], nil
+}
+
+//查看队列所有数据
+func (queue *Queue) showQueue() {
+	for i := queue.front + 1; i <= queue.rear; i++ {
+		fmt.Printf("queue[%d]%d\n", i, queue.queue[i])
+	}
+	fmt.Println()
 }
