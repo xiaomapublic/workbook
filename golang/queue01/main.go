465c6355362af43fa88a79fce783bd7757d41a0d
maya <maya1759@stnts.com>
2020-05-09 19:13:16 +0800

    queue02

--- golang/queue01/main.go
+++ golang/queue01/main.go
@@ -6,25 +6,27 @@ import (
 	"os"
 )
 
+// 简单数组队列,未使用环形数组,性能不好
 type Queue struct {
-	maxSize int
-	front   int
-	rear    int
-	queue   []int
+	maxSize int   //队列最大容量
+	rear    int   //当前下标
+	queue   []int //存储队列数据
 }
 
 //切片实现队列
 func main() {
+	fmt.Printf("%s: ", "队列启动，请输入队列容量")
+	var maxSize int
+	fmt.Scanln(&maxSize)
 	queue := &Queue{
-		maxSize: 5,
-		front:   -1,
+		maxSize: maxSize,
 		rear:    -1,
 		queue:   make([]int, 0),
 	}
 
 	var key string
 	for {
-		fmt.Printf("front: %d, rear: %d \n", queue.front, queue.rear)
+		fmt.Printf("参数信息 queue: %+v \n", queue)
 		fmt.Println("1、添加数据入队列，请输入add")
 		fmt.Println("2、获取队列数据，请输入get")
 		fmt.Println("3、展示队列，请输入show")
@@ -67,16 +69,20 @@ func (queue *Queue) addQueue(data int) (error error) {
 
 //队列弹出数据
 func (queue *Queue) getQueue() (data int, err error) {
-	if queue.front == queue.rear {
+	if queue.rear == -1 {
 		return -1, errors.New("queue empty")
 	}
-	queue.front++
-	return queue.queue[queue.front], nil
+	data = queue.queue[0]
+	//将切片下标为0的数据删除,大批量数据会存在性能问题
+	queue.queue = append(queue.queue[:0], queue.queue[1:]...)
+	//下标回退
+	queue.rear--
+	return data, nil
 }
 
 //查看队列所有数据
 func (queue *Queue) showQueue() {
-	for i := queue.front + 1; i <= queue.rear; i++ {
+	for i := 0; i <= queue.rear; i++ {
 		fmt.Printf("queue[%d]%d\n", i, queue.queue[i])
 	}
 	fmt.Println()
